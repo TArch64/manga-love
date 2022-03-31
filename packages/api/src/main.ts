72f55e919335f3c15564943cc4481061fd484b41
TArch64 <tarasturchenko1551@gmail.com>
2022-03-31 20:46:57 +0300

    setup docker

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { API_PORT, API_HOST } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(Number(API_PORT), API_HOST, () => {
+      console.log(`Server started at http://${API_HOST}:${API_PORT}`);
+  });
 }
 bootstrap();
